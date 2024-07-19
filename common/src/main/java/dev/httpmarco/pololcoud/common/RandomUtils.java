eb8afc2a5c295cf7f512fa19e32516a289c2d1e1
jirmjahu <159639233+jirmjahu@users.noreply.github.com>
2024-07-20 00:58:17 +0200

    make final

--- common/src/main/java/dev/httpmarco/pololcoud/common/RandomUtils.java
+++ common/src/main/java/dev/httpmarco/pololcoud/common/RandomUtils.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class RandomUtils {
 
-    private static Random RANDOM = new Random();
+    private static final Random RANDOM = new Random();
 
     public static int getRandomNumber(int max) {
         return RANDOM.nextInt(max);

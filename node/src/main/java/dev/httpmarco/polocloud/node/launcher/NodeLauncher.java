2e10f8ebe6f6ea53a03b4809fa028a90ae92b646
RECHERGG <rechergg@gmail.com>
2024-08-02 16:36:03 +0200

    code cleanup

--- node/src/main/java/dev/httpmarco/polocloud/node/launcher/NodeLauncher.java
+++ node/src/main/java/dev/httpmarco/polocloud/node/launcher/NodeLauncher.java
@@ -7,14 +7,14 @@ import dev.httpmarco.polocloud.node.Node;
 public class NodeLauncher {
     public static void main(String[] args) {
 
+        var slf4jDependency = new Dependency("org.slf4j", "slf4j-api", "2.0.13");
+        var log4jApiDependency = new Dependency("org.apache.logging.log4j", "log4j-api", "2.23.1");
+        var log4jCoreDependency = new Dependency("org.apache.logging.log4j", "log4j-core", "2.23.1");
+        var log4jSlf4jDependency = new Dependency("org.apache.logging.log4j", "log4j-slf4j2-impl", "2.23.1");
+        var jlineDependency = new Dependency("org.jline", "jline", "3.26.3");
+
         // load logging dependencies
-        DependencyDownloader.download(
-                new Dependency("org.slf4j", "slf4j-api", "2.0.13"),
-                new Dependency("org.apache.logging.log4j", "log4j-api", "2.23.1"),
-                new Dependency("org.apache.logging.log4j", "log4j-core", "2.23.1"),
-                new Dependency("org.apache.logging.log4j", "log4j-slf4j2-impl", "2.23.1"),
-                new Dependency("org.jline", "jline", "3.26.3")
-        );
+        DependencyDownloader.download(slf4jDependency, log4jApiDependency, log4jCoreDependency, log4jSlf4jDependency, jlineDependency);
 
         try {
             System.setProperty("startup", String.valueOf(System.currentTimeMillis()));

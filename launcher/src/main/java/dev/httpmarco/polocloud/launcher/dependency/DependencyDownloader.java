c52784f81208d78b49e35579e8f64ffbca25ff3f
RECHERGG <rechergg@gmail.com>
2024-08-03 16:03:00 +0200

    fixed error

--- launcher/src/main/java/dev/httpmarco/polocloud/launcher/dependency/DependencyDownloader.java
+++ launcher/src/main/java/dev/httpmarco/polocloud/launcher/dependency/DependencyDownloader.java
@@ -4,6 +4,7 @@ import dev.httpmarco.polocloud.launcher.PoloCloudLauncher;
 import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
 
+import java.io.File;
 import java.nio.file.Path;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
@@ -27,6 +28,13 @@ public class DependencyDownloader {
     public void download(Dependency dependency) {
         var file = DOWNLOAD_DIR.resolve(dependency + ".jar").toFile();
 
+        if (dependency.withSubDependencies()) {
+            dependency.loadSubDependencies();
+            for (var subDependency : dependency.subDependencies()) {
+                download(subDependency);
+            }
+        }
+
         if (file.exists()) {
             PoloCloudLauncher.CLASS_LOADER.addURL(file.toURI().toURL());
             return;
@@ -34,13 +42,6 @@ public class DependencyDownloader {
 
         DependencyHelper.download(dependency.downloadUrl(), file);
         PoloCloudLauncher.CLASS_LOADER.addURL(file.toURI().toURL());
-
-        if (dependency.withSubDependencies()) {
-            dependency.loadSubDependencies();
-            for (var subDependency : dependency.subDependencies()) {
-                download(subDependency);
-            }
-        }
     }
 
     public static void download(Dependency... dependencies) {
@@ -51,22 +52,32 @@ public class DependencyDownloader {
         int totalDependencies = dependencies.size();
         for (int i = 0; i < totalDependencies; i++) {
             var dependency = dependencies.get(i);
-            logProgress(totalDependencies, i + 1, dependency.artifactId());
+            logProgress(totalDependencies, i + 1, dependency);
             download(dependency);
         }
 
         clearTerminal();
     }
 
-    private void logProgress(int total, int current, String name) {
+    private void logProgress(int total, int current, Dependency dependency) {
         var time = LocalTime.now();
         var formattedTime = DateTimeFormatter.ofPattern("HH:mm:ss").format(time);
-        System.out.printf("\r%s \u001B[90m| \u001B[36mINFO\u001B[90m:\u001B[0m Downloading Dependencies - %s (\u001B[36m%d\u001B[0m/\u001B[36m%d\u001B[0m)", formattedTime, name, current, total);
+
+        if (findDependency(dependency).exists()) {
+            System.out.printf("\r%s \u001B[90m| \u001B[36mINFO\u001B[90m:\u001B[0m Found Dependency - %s (\u001B[36m%d\u001B[0m/\u001B[36m%d\u001B[0m)", formattedTime, dependency.artifactId(), current, total);
+            return;
+        }
+
+        System.out.printf("\r%s \u001B[90m| \u001B[36mINFO\u001B[90m:\u001B[0m Downloading Dependencies - %s (\u001B[36m%d\u001B[0m/\u001B[36m%d\u001B[0m)", formattedTime, dependency.artifactId(), current, total);
     }
 
     private void clearTerminal() {
         System.out.print("\r");
         System.out.print(" ".repeat(80));
         System.out.print("\r");
+    }
+
+    private File findDependency(Dependency dependency) {
+        return DOWNLOAD_DIR.resolve(dependency + ".jar").toFile();
     }
 }
